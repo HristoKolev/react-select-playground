078f76b299d1754d5dc8953d18d4d4139e9d7196
Hristo Kolev <hristo.kolev@interop.io>
2023-08-11 16:00:05 +0300

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,74 @@
-import { type JSX, useEffect, useState } from 'react';
+import type { JSX } from 'react';
+import { useState } from 'react';
+import Select from 'react-select';
 
-import logoUrl from 'src/logo.png';
-import { formatDate } from '~helpers';
+type CustomOption = { label: string } & (
+  | { value: string; selected?: boolean }
+  | { options: CustomOption[] }
+);
+
+const defaultOptions: CustomOption[] = [
+  { label: 'Cat 1', value: 'cat-1' },
+  { label: 'Cat 2', value: 'cat-2' },
+  {
+    label: 'Group 1',
+    options: [
+      { label: 'Cat 3', value: 'cat-3', selected: true },
+      { label: 'Cat 4', value: 'cat-4' },
+    ],
+  },
+];
+
+const getSelected = (options: CustomOption[]): CustomOption[] => {
+  const result: CustomOption[] = [];
+  const stack = [options];
+  while (stack.length) {
+    const opt = stack.pop() as CustomOption[];
+    for (const element of opt) {
+      if ('options' in element) {
+        stack.push(element.options);
+      } else if (element.selected) {
+        result.push(element);
+      }
+    }
+  }
+  return result;
+};
+
+const selectOption = (options: CustomOption[], value: string) => {
+  const stack = [options];
+  while (stack.length) {
+    const opt = stack.pop() as CustomOption[];
+    for (const element of opt) {
+      if ('options' in element) {
+        stack.push(element.options);
+      } else {
+        element.selected = element.value === value;
+      }
+    }
+  }
+};
 
 export const App = (): JSX.Element => {
-  const [message, setMessage] = useState<string | undefined>();
+  const [options, setOptions] = useState<CustomOption[]>(defaultOptions);
+  const value = getSelected(options);
 
-  useEffect(() => {
-    fetch('http://localhost:3001/')
-      .then((res) => res.text())
-      .then(setMessage)
-      // eslint-disable-next-line no-console
-      .catch(console.error);
-  }, []);
+  // eslint-disable-next-line no-console
+  console.log('HKOLEV: App.tsx | options', JSON.stringify(options, null, 2));
 
   return (
     <div className="text text-center">
-      <div>Hello Vite</div>
-      <div data-testid="date-label">{formatDate(new Date())}</div>
-      {message && <div data-testid="server-message">{message}</div>}
-      <div>
-        <img src={logoUrl} alt="logo" />
-      </div>
+      <Select
+        options={options}
+        value={value}
+        onChange={(selectedOption) => {
+          if (selectedOption && 'value' in selectedOption) {
+            const clonedOptions = structuredClone(options);
+            selectOption(clonedOptions, selectedOption.value);
+            setOptions(clonedOptions);
+          }
+        }}
+      />
     </div>
   );
 };
